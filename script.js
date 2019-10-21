9c85df1de547d9156d0751e4005e6552ad2eb74f
lhlavati <lukahlavati@gmail.com>
2019-10-21 02:08:02 +0200

    Dodan script.js i implementirana metoda za dohvaćanje svih proizvoda

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Radi");
+const clearCartBtn = document.querySelector('.clear-cart')
+const cartDOM = document.querySelector('.cart')
+const cartItems = document.querySelector('.cart-items')
+const cartTotal = document.querySelector('.cart-total')
+const cartContent = document.querySelector('.cart-content')
+const cartContainer = document.querySelector('.container')
+
+let cart = [];
+class Products{
+    async getProducts(){
+        try {
+            let result = await fetch("products.json");
+            return result;
+        } catch (error) {
+            console.log(error);
+        }
+        
+    }
+}
+
+class UI{
+
+}
+
+class Storage{
+
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const ui = new UI()
+    const products = new Products();
+    products.getProducts().then(data => console.log(data));
+})
