dc506fb90d0f4427ad44737e904049b6e5ab90dc
lhlavati <lukahlavati@gmail.com>
2019-10-23 23:21:43 +0200

    Dodana metoda za računanje ukupnog iznosa košarice i dodavanje proizvoda u košaricu

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const clearCartBtn = document.querySelector('.clear-cart');
-const cartDOM = document.querySelector('.cart');
-const cartItems = document.querySelector('.cart-items');
-const cartTotal = document.querySelector('.cart-total');
-const cartContent = document.querySelector('.cart-content');
-const cartContainer = document.querySelector('.container');
+const cartTotal = document.getElementsByClassName(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+console.log(cartContent);
+const cartContainer = document.querySelector(".container");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
 const productsDOM = document.querySelector(".products-left");
 
 let cart = [];
@@ -11,85 +11,114 @@ let cart = [];
 let buttonsDOM = [];
 
 class Products {
-    async getProducts() {
-        try {
-            let result = await fetch("products.json");
-            let data = await result.json();
-            let products = data.products;
-            products = products.map(item => {
-                const { id } = item;
-                const { name } = item;
-                const { image } = item;
-                const { amount, currency, measureUnit } = item.price;
-                return { id, name, image, amount, currency, measureUnit }
-            })
-            return products
-        } catch (error) {
-            console.log(error);
-        }
-
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+      let products = data.products;
+      products = products.map(item => {
+        const { id } = item;
+        const { name } = item;
+        const { image } = item;
+        const { amount, currency, measureUnit } = item.price;
+        return { id, name, image, amount, currency, measureUnit };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
 
 class UI {
-    // displayProducts(products){
-    //     let result = "";
-    //     products.forEach(product => {
-    //         result += `
-    //         <article class="product">
-    //             <div class="img-containter">
-    //                 <img src=${product.image}
-    //                     alt="product" class="product-img">
-    //             </div>
-    //             <h3>${product.name}</h3>
-    //             <h4>${product.amount} ${product.currency}/${product.measureUnit}</h4>
-    //             <a class="waves-effect waves-light red btn" id="dodaj" data-id=${product.id}><i
-    //                     class="material-icons left">add_circle</i>Dodaj</a>
-    //         </article>
-    //         `;
-    //     });
-    //     productsDOM.innerHTML = result;
-    // }
-    getButtons() {
-        const buttons = [...document.querySelectorAll(".add-btn")];
-        buttonsDOM = buttons;
-        buttons.forEach(button => {
-            let id = button.dataset.id;
-            console.log(id);
-            let inCart = cart.find(item => item.id === id);
-            console.log(inCart);
-            if (inCart) {
-                button.innerText = "U košarici";
-                button.disabled = true;
-            }
-            button.addEventListener("click", event => {
-                console.log(event);
-                event.target.innerText = "U košarici";
-                event.target.disabled = true;
-                let cartItem = Storage.getProduct(id);
-                console.log(cartItem);
-            });
-        });
-    }
+  // displayProducts(products){
+  //     let result = "";
+  //     products.forEach(product => {
+  //         result += `
+  //         <article class="product">
+  //             <div class="img-containter">
+  //                 <img src=${product.image}
+  //                     alt="product" class="product-img">
+  //             </div>
+  //             <h3>${product.name}</h3>
+  //             <h4>${product.amount} ${product.currency}/${product.measureUnit}</h4>
+  //             <a class="waves-effect waves-light red btn" id="dodaj" data-id=${product.id}><i
+  //                     class="material-icons left">add_circle</i>Dodaj</a>
+  //         </article>
+  //         `;
+  //     });
+  //     productsDOM.innerHTML = result;
+  // }
+  getButtons() {
+    const buttons = [...document.querySelectorAll(".add-btn")];
+    buttonsDOM = buttons;
+    buttons.forEach(button => {
+      let id = button.dataset.id;
+      let inCart = cart.find(item => item.id == id);
+      if (inCart) {
+        button.innerText = "U košarici";
+        button.disabled = true;
+      }
+      button.addEventListener("click", event => {
+        event.target.innerText = "U košarici";
+        event.target.disabled = true;
+        let cartItem = { ...Storage.getProduct(id), count: 1 };
+        cart = [...cart, cartItem];
+        Storage.saveCart(cart);
+        this.setCartValues(cart);
+        this.addCartItem(cartItem);
+      });
+    });
+  }
+  setCartValues(cart) {
+    let tempTotal = 0;
+    cart.map(item => {
+      tempTotal += item.amount * item.count;
+    });
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    console.log(cartTotal);
+  }
+  addCartItem(item) {
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+    console.log(div);
+    div.innerHTML = `<img src=${item.image} alt="product">
+                    <div>
+                        <h4>${item.name}</h4>
+                        <h5>${item.amount} ${item.currency}</h5>
+                        <span class="remove-item" data-id=${item.id}><i class="material-icons">remove_circle</i></span>
+                    </div>
+                    <div class="product-count">
+                        <i class="material-icons arrow_up" data-id=${item.id}>arrow_drop_up</i>
+                        <p class="item-amount">${item.count}</p>
+                        <i class="material-icons arrow_down" data-id=${item.id}>arrow_drop_down</i>
+                    </div>`;
+    cartContent.append(div);
+    console.log(cartContent);
+  }
 }
 
 class Storage {
-    static saveProducts(products) {
-        localStorage.setItem("products", JSON.stringify(products));
-    }
-    static getProduct(id) {
-        let products = JSON.parse(localStorage.getItem("products"));
-        return products.find(product => product.id === id);
-    }
+  static saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem("products"));
+    return products.find(product => product.id == id);
+  }
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const ui = new UI();
-    const products = new Products();
-    products.getProducts().then(products => {
-        // ui.displayProducts(products);
-        Storage.saveProducts(products);
-    }).then(() => {
-        ui.getButtons();
+  const ui = new UI();
+  const products = new Products();
+  products.getProducts().then(products => {
+      // ui.displayProducts(products);
+      Storage.saveProducts(products);
+    })
+    .then(() => {
+      ui.getButtons();
     });
 });
